118626624868fbdca696a08de9a4ee245ff9ec57
etornam19 <eadevor@hawk.iit.edu>
2025-05-06 10:08:34 -0500

    updated js file to send email confirmation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,17 @@
 function mail(){
     let params = {
-        fname: document.getElementById("myFName").value,
-        lname: document.getElementById("myLName").value,
+        first_name: document.getElementById("myFName").value,
+        last_name: document.getElementById("myLName").value,
         catagory: document.getElementById("catagory").value,
         answer: document.getElementById("answer").value,
         message: document.getElementById("message").value,
 
-    }
+    };
 }
+
+const serviceID = "service_ruy9xnn";
+const templateID ="template_6qiougq";
+
+emailjs.send(serviceID,templateID,params).then(alert("Email Sent!")
+
+)
