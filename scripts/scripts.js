81b2559d9b6bcecac243bbff0d7a2168daf91cc2
etornam19 <eadevor@hawk.iit.edu>
2025-05-06 09:12:03 -0500

    added viewport and updates script

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+function mail(){
+    let params = {
+        fname: document.getElementById("myFName").value,
+        lname: document.getElementById("myLName").value,
+        catagory: document.getElementById("catagory").value,
+        answer: document.getElementById("answer").value,
+        message: document.getElementById("message").value,
+
+    }
+}
