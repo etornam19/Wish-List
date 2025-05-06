d6d9c789287430c8d4a827a1f36e642266d1e847
etornam19 <eadevor@hawk.iit.edu>
2025-05-06 13:51:51 -0500

    updated form information

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,14 +13,12 @@ function mail(){
     let params = {
         first_name: document.getElementById("myFName").value,
         last_name: document.getElementById("myLName").value,
-        catagory: document.getElementById("catagory").value,
-        answer: document.getElementById("answerY").value,
-        info: document.getElementById("giftInfo").value,
+        category: document.getElementById("catagory").value,
+        answer: document.getElementById("giftInfo").value,
         answer2: document.getElementById("answerN").value,
         message: document.getElementById("message").value,
 
     };
-
 
     const serviceID = "service_ruy9xnn";
     const templateID ="template_6qiougq";
